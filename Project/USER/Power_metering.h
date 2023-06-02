e3b8416f8a156597b336ddadf46250f0d6e73790
yuvy <2628587386@qq.com>
2023-06-02 11:55:19 +0800

    dev:[驱动编写,编写完成]

---
+++ Project/USER/Power_metering.h
@@ -0,0 +1,11 @@
+#ifndef __POWER_METERING_H
+#define __POWER_METERING_H
+
+#include "gd32f10x.h"
+
+void power_metering_init(void);
+
+
+extern uint16_t adc_value[2];
+
+#endif /* __POWER_METERING_H */
